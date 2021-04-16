d07c5585f7a2948aab8f23d8ce0db70c4f655815
Kleo <kleo@hasani>
2021-04-15 23:08:06 -0400

    bug fixes

--- src/core/Storage.js
+++ src/core/Storage.js
@@ -6,143 +6,145 @@ const READ_ONLY = 0o444;
 const READ_WRITE = 0o666;
 
 class Storage {
-  /**
-   * @param {string} path
-   */
-  constructor(path, defaults = []) {
-    if (!path) throw new Error("Path was not supplied");
-    this._path = path;
-    this._storage = defaults;
-    this._isSaved = false;
-  }
+	/**
+	 * @param {string} path
+	 */
+	constructor(path, defaults = []) {
+		if (!path) throw new Error("Path was not supplied");
+		this._path = path;
+		this._storage = defaults;
+		this._isSaved = false;
+	}
 
-  /**
-   * Load JSON into in-memory store
-   * @returns {Promise<void>}
-   */
-  load() {
-    return new Promise((accept, reject) => {
-      access(this._path)
-        .catch(() => {
-          writeFile(this._path, JSON.stringify(this._storage), {
-            encoding: "utf-8",
-            flag: "w",
-            mode: READ_ONLY,
-          }).catch((err) => reject(err));
-        })
-        .finally(() => {
-          readFile(this._path, {
-            encoding: "utf-8",
-            flag: "r",
-          })
-            .then((data) => {
-              this._storage = JSON.parse(data);
-              this._isSaved = true;
-              accept();
-            })
-            .catch(() => reject());
-        });
-    });
-  }
+	/**
+	 * Load JSON into in-memory store
+	 * @returns {Promise<void>}
+	 */
+	load() {
+		return new Promise((accept, reject) => {
+			access(this._path)
+				.catch(() => {
+					writeFile(this._path, JSON.stringify(this._storage), {
+						encoding: "utf-8",
+						flag: "w",
+						mode: READ_ONLY,
+					}).catch((err) => reject(err));
+				})
+				.finally(() => {
+					readFile(this._path, {
+						encoding: "utf-8",
+						flag: "r",
+					})
+						.then((data) => {
+							this._storage = JSON.parse(data);
+							this._isSaved = true;
+							accept();
+						})
+						.catch(() => reject());
+				});
+		});
+	}
 
-  /**
-   * Get length of store
-   * @returns {number}
-   */
-  get length() {
-    return this._storage.length;
-  }
+	/**
+	 * Get length of store
+	 * @returns {number}
+	 */
+	get length() {
+		return this._storage.length;
+	}
 
-  get items() {
-    return this._storage.values();
-  }
+	get items() {
+		return this._storage.values();
+	}
 
-  /**
-   * Set item in memory store
-   * @param {object} item
-   * @param {any} item.key
-   * @param {any} item.value
-   */
-  setItem(item) {
-    if (!item) throw new Error("No item provided");
-    const found_item = this._storage.find((itm) => itm.key === item.key);
-    if (found_item) found_item.value = item.value;
-    else this._storage.push(item);
-    this._isSaved = false;
-  }
+	/**
+	 * Set item in memory store
+	 * @param {object} item
+	 * @param {any} item.key
+	 * @param {any} item.value
+	 */
+	setItem(item) {
+		if (!item) throw new Error("No item provided");
+		const found_item = this._storage.find((itm) => itm.key === item.key);
+		if (found_item) found_item.value = item.value;
+		else this._storage.push(item);
+		this._isSaved = false;
+	}
 
-  /**
-   * Retrieve item by key from in-memory store
-   * return object or undefined if not found
-   * @param {any} key
-   * @returns {object | undefined}
-   */
-  getItem(key) {
-    if (!key) throw new Error("No key provided");
-    return this._storage.find((itm) => itm.key === key);
-  }
+	/**
+	 * Retrieve item by key from in-memory store
+	 * return object or undefined if not found
+	 * @param {any} key
+	 * @returns {object | undefined}
+	 */
+	getItem(key) {
+		if (!key) throw new Error("No key provided");
+		return this._storage.find((itm) => itm.key === key);
+	}
 
-  /**
-   * Remove item from in-memory store
-   * @param {any} key
-   * @returns {void}
-   */
-  removeItem(key) {
-    if (!key) throw new Error("No key provided");
-    this._storage = this._storage.filter((itm) => itm.key !== key);
-    this._isSaved = false;
-  }
+	/**
+	 * Remove item from in-memory store
+	 * @param {any} key
+	 * @returns {void}
+	 */
+	removeItem(key) {
+		if (!key) throw new Error("No key provided");
+		this._storage = this._storage.filter((itm) => itm.key !== key);
+		this._isSaved = false;
+	}
 
-  /**
-   * Clear in-memory store
-   * @returns {void}
-   */
-  clear() {
-    this._storage = [];
-    this._isSaved = false;
-  }
+	/**
+	 * Clear in-memory store
+	 * @returns {void}
+	 */
+	clear() {
+		this._storage = [];
+		this._isSaved = false;
+	}
 
-  /**
-   * Save in-memory store to JSON file
-   * @returns {Promise<void> | void}
-   */
-  save() {
-    if (!this._isSaved)
-      return new Promise((accept, reject) => {
-        chmod(this._path, READ_WRITE)
-          .then(() => {
-            writeFile(this._path, JSON.stringify(this._storage), {
-              encoding: "utf-8",
-              flag: "w",
-            })
-              .catch((err) => reject(err))
-              .finally(() => {
-                chmod(this._path, READ_ONLY)
-                  .then(() => {
-                    this._isSaved = true;
-                    accept();
-                  })
-                  .catch((err) => reject(err));
-              });
-          })
-          .catch((err) => reject(err));
-      });
-  }
+	/**
+	 * Save in-memory store to JSON file
+	 * @returns {Promise<void> | void}
+	 */
+	save() {
+		if (!this._isSaved) {
+			this._storage = this._storage.filter((items) => items.value.body !== "");
+			return new Promise((accept, reject) => {
+				chmod(this._path, READ_WRITE)
+					.then(() => {
+						writeFile(this._path, JSON.stringify(this._storage), {
+							encoding: "utf-8",
+							flag: "w",
+						})
+							.catch((err) => reject(err))
+							.finally(() => {
+								chmod(this._path, READ_ONLY)
+									.then(() => {
+										this._isSaved = true;
+										accept();
+									})
+									.catch((err) => reject(err));
+							});
+					})
+					.catch((err) => reject(err));
+			});
+		}
+	}
 
-  /**
-   * Delete JSON file
-   * @returns {Promise<boolean>}
-   */
-  delete() {
-    return new Promise((accept, reject) => {
-      rm(this._path)
-        .then(() => {
-          this._isSaved = false;
-          accept(true);
-        })
-        .catch(() => reject(false));
-    });
-  }
+	/**
+	 * Delete JSON file
+	 * @returns {Promise<boolean>}
+	 */
+	delete() {
+		return new Promise((accept, reject) => {
+			rm(this._path)
+				.then(() => {
+					this._isSaved = false;
+					accept(true);
+				})
+				.catch(() => reject(false));
+		});
+	}
 }
 
 module.exports = { Storage };
--- views/note/render.js
+++ views/note/render.js
@@ -6,89 +6,65 @@ const btnNew = document.querySelector("#btn-new");
 const btnPin = document.querySelector("#btn-pin");
 const btnClose = document.querySelector("#btn-close");
 const txtArea = document.querySelector("#txt-area");
-const btnCheck = document.querySelector("#btn-check");
-const btnList = document.querySelector("#btn-list");
 
 /**
  * @param {string} cmd
  * @param {string} value
  */
 function format(cmd, value = null) {
-  if (!cmd) throw new Error("No command passed");
-  txtArea.focus();
-  return document.execCommand(cmd, false, value);
+	if (!cmd) throw new Error("No command passed");
+	txtArea.focus();
+	return document.execCommand(cmd, false, value);
 }
 
 // electron API listeners
 window.electron.ipcOnce("window:ready", (e, data) => {
-  const { uid, isPinned, body } = data;
-  if (!data)
-    throw new Error("Unable to open window. Window data was not provided");
-  _uid = uid;
-  btnPin.className = isPinned ? "btn btn-pin-active" : "btn";
-  txtArea.innerHTML = body;
+	const { uid, isPinned, body } = data;
+	if (!data) throw new Error("Unable to open window. Window data was not provided");
+	_uid = uid;
+	btnPin.className = isPinned ? "btn btn-pin-active" : "btn";
+	txtArea.innerHTML = body;
 });
 
 window.electron.ipcOnce("window:closed", () => {
-  window.electron.ipcRemoveAllListeners();
-  window.close();
+	window.electron.ipcRemoveAllListeners();
+	window.close();
 });
 
 window.electron.ipcOn("render:window-update-btnPin", (e, data) => {
-  const { isPinned } = data;
-  if (!data)
-    throw new Error(
-      "Unable to open window. Window pinned status was not provided",
-    );
-  btnPin.className = isPinned ? "btn btn-pin-active" : "btn";
+	const { isPinned } = data;
+	if (!data) throw new Error("Unable to open window. Window pinned status was not provided");
+	btnPin.className = isPinned ? "btn btn-pin-active" : "btn";
 });
 
 // render listeners
 btnNew.onclick = () => {
-  window.electron.ipcSend("window:new");
+	window.electron.ipcSend("window:new");
 };
 
 btnPin.onclick = () => {
-  window.electron.ipcSend("window:pin", { _uid });
+	window.electron.ipcSend("window:pin", { uid: _uid });
 };
 
 btnClose.onclick = () => {
-  window.electron.ipcSend("window:close", {
-    _uid,
-    isPinned: btnPin.classList.contains("btn-pin-active"),
-    body: txtArea.innerHTML,
-  });
-};
-
-btnCheck.onclick = () => {
-  format("strikethrough");
-  document.getSelection().collapseToEnd();
-  btnCheck.classList = "btn-style active";
-};
-
-btnList.onclick = () => {
-  format("insertorderedlist");
+	window.electron.ipcSend("window:close", {
+		uid: _uid,
+		isPinned: btnPin.classList.contains("btn-pin-active"),
+		body: txtArea.innerHTML,
+	});
 };
 
 txtArea.onkeydown = (e) => {
-  if (e.which === 9) {
-    e.preventDefault();
-    format("insertHTML", "&emsp;");
-  }
+	if (e.which === 9) {
+		e.preventDefault();
+		format("insertHTML", "&emsp;");
+	}
 
-  if (e.which > 36 && e.which < 41) {
-    const node = document.getSelection().anchorNode.parentNode;
-    if (node.nodeName === "STRIKE")
-      if (btnCheck.classList.contains("active")) return;
-      else btnCheck.classList = "btn-style active";
-    else btnCheck.classList = "btn-style";
-  }
-};
-
-txtArea.onselectstart = (e) => {
-  const node = e.target.parentNode;
-  if (node.nodeName === "STRIKE")
-    if (btnCheck.classList.contains("active")) return;
-    else btnCheck.classList = "btn-style active";
-  else btnCheck.classList = "btn-style";
+	// if (e.which > 36 && e.which < 41) {
+	//   const node = document.getSelection().anchorNode.parentNode;
+	//   if (node.nodeName === "STRIKE")
+	//     if (btnCheck.classList.contains("active")) return;
+	//     else btnCheck.classList = "btn-style active";
+	//   else btnCheck.classList = "btn-style";
+	// }
 };
