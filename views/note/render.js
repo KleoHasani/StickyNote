c0f090bcd07c98c1706940c11f502695d40d3f47
Kleo <kleo@hasani>
2021-04-26 21:58:55 -0400

    added formating for list and strike, added UI alert for strike formating when active

--- views/note/render.js
+++ views/note/render.js
@@ -7,6 +7,7 @@ const btnList = document.querySelector("#button-list");
 const btnStrike = document.querySelector("#button-strike");
 
 /**
+ * Format selected element with passed command
  * @param {string} cmd
  * @param {string} value
  */
@@ -14,6 +15,15 @@ function format(cmd, value = null) {
 	if (!cmd) throw new Error("No command passed");
 	txtArea.focus();
 	return document.execCommand(cmd, false, value);
+}
+
+/**
+ * Format element to have active style
+ * @param {boolean} isActive
+ * @param {Node} element
+ */
+function toggleIsActive(isActive, element) {
+	element.classList = isActive ? "active" : "";
 }
 
 // electron API listeners
@@ -34,6 +44,8 @@ window.electron.ipcOnce("window:closed", () => {
 });
 
 // render listeners
+
+// Insert Tab on tab button press
 txtArea.onkeydown = (e) => {
 	if (e.which === 9) {
 		e.preventDefault();
@@ -41,10 +53,24 @@ txtArea.onkeydown = (e) => {
 	}
 };
 
+// Toggle formated button active on key up
+txtArea.onkeyup = (e) => {
+	toggleIsActive(document.queryCommandState("strikethrough"), btnStrike);
+};
+
+// Toggle formated button active on select or cursor position set on mouse click
+txtArea.onclick = () => {
+	toggleIsActive(document.queryCommandState("strikethrough"), btnStrike);
+};
+
+// Insert List
 btnList.onclick = () => {
 	format("insertOrderedList");
 };
 
+// Strike text out
 btnStrike.onclick = () => {
-	format("strikeThrough");
+	format("strikethrough");
+	document.getSelection().collapseToEnd();
+	toggleIsActive(document.queryCommandState("strikethrough"), btnStrike);
 };
